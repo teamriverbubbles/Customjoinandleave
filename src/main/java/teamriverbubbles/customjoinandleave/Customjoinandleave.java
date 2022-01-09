c8c4a62d5203a3f7a409f3462aee10e645b74679
Xavier horwood <39760456+Xavierhorwood@users.noreply.github.com>
2022-01-09 22:13:21 +1000

    Create Customjoinandleave.java

---
+++ src/main/java/teamriverbubbles/customjoinandleave/Customjoinandleave.java
@@ -0,0 +1,72 @@
+package teamriverbubbles.customjoinandleave;
+
+import me.clip.placeholderapi.PlaceholderAPI;
+import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Customjoinandleave extends JavaPlugin implements Listener {
+
+    FileConfiguration config = getConfig();
+    boolean enablePlaceholderAPI = true;
+
+    @Override
+    public void onEnable() {
+        config.addDefault("CommandToRun", "say Hello Player!");
+        config.options().copyDefaults(true);
+        saveConfig();
+
+        if (Bukkit.getPluginManager().getPlugin("PlaceholderAPI") != null) {
+            /*
+             * We register the EventListener here, when PlaceholderAPI is installed.
+             * Since all events are in the main class (this class), we simply use "this"
+             */
+            Bukkit.getPluginManager().registerEvents(this, this);
+        } else {
+            /*
+             * We inform about the fact that PlaceholderAPI isn't installed and then
+             * disable this plugin to prevent issues.
+             */
+            enablePlaceholderAPI = false;
+            getLogger().warning("Could not find PlaceholderAPI! This plugin is required.");
+        }
+    }
+
+    // This method checks for incoming players and sends them a message
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        //if player has permission exempt
+        if (event.getPlayer().hasPermission("customjoinandleave.exempt")) {
+            return;
+        }
+
+        Player player = event.getPlayer();
+        String command = config.getString("CommandToRun");
+
+        //if PlaceholderAPI is enabled
+        assert command != null;
+        if(!enablePlaceholderAPI) {
+            //replace placeholders with values and send message
+            command = command.replace("%player_name%", player.getName());
+        }
+        else
+        {
+            //set placeholders and run command
+            command = PlaceholderAPI.setPlaceholders(event.getPlayer(), command);
+        }
+        player.performCommand(command);
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+        //save config
+        saveConfig();
+        //print to console
+        getLogger().info("Plugin has been disabled!");
+    }
+}
